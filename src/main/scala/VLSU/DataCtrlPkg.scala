8c0f2fbb1ff6065f9768a1bcc1086f5169642d89
JTZ677 <s2481455724@163.com>
2025-04-19 12:51:46 +0800

    [v1] [vlsu/DataCtrlPkg] [D] Finish shuffle logic

---
+++ src/main/scala/VLSU/DataCtrlPkg.scala
@@ -0,0 +1,304 @@
+package vlsu
+
+import chisel3._
+import chisel3.util._
+import org.chipsalliance.cde.config.Parameters
+import scala.collection.immutable.ListMap
+
+/** The Trait to help shuffle and deshuffle.
+ *
+ *  1. SHUFFLE is an important concept in the process of memory access in vector processors.
+ *  After the data is shuffled, it will be evenly distributed among the lanes to
+ *  'maximize the utilization' of hardware resources and 'improve parallelism'.
+ *
+ * The shuffle function has 2 Inputs and 2 Outputs:
+ *
+ *        EW ----> |-----------------| ----> lane_LUT
+ *                 |    shuffle      |
+ *  seqHbIdx ----> |-----------------| ----> laneOffset_LUT
+ *
+ * @param EW: OntHot EW = 4/8/16/32
+ * @param idx: The half byte index before shuffling
+ * @param lane: In which lane does the half byte locate after shuffling
+ * @param laneOffset: Half byte's offset in the lane
+ *
+ * seqIdx is composed of the following parts (Doesn't have elemOff when ew = 4):
+ *         | <-------------------   hbIdxBits    --------------------> |
+ *         | <- elemIdxBits -> | <- laneIdBits -> |  <- elemOffBits -> |
+ *
+ * seqIdx: |      elemIdx      |      laneId      |       elemOff      |
+ *
+ *                             | <----------- laneIdOff -------------> |
+ *
+ * The position of laneId and elemIdx is SWITCHED in the 2D Cln mode:
+ *         | <-------------------   hBIdxBits    --------------------> |
+ *         | <- laneIdBits -> | <- elemIdxBits -> |  <- elemOffBits -> |
+ *
+ * seqIdx: |      laneId      |      elemIdx      |       elemOff      |
+ *
+ *                            |  <----------- elemIdxOff -------------> |
+ *
+ * 2. DESHUFFLE is the inverse process of shuffle (shfHbIdx -> seqHbIdx)
+ */
+trait SuffleHelper {
+  // Can only be inherited by classes or traits that inherit from VLSUModule
+  self: VLSUModule =>
+
+// ------------------------------------------ Parameters ------------------------------------------------- //
+  val hbNum      : Int = (SLEN / 4) * NrLanes // halfByte number in SLEN * NrLanes
+  val hbIdxBits  : Int = log2Ceil(hbNum)
+  val laneIdBits : Int = log2Ceil(NrLanes)
+  val laneOffBits: Int = log2Ceil(SLEN/4)
+
+  private val laneElemNum = SLEN / possibleEWs.min // Max element number in a lane, typically 128/4 = 32
+  private val seqHbIdxs   = 0 until hbNum
+  private val seqElemIdxs = 0 until laneElemNum
+
+  private val seqIdxs: Seq[seqHbIdx] = (0 until hbNum).map(i => new seqHbIdx(i))
+  private val shfElemIdxMap = shuffle_elemIdx
+
+  private val seq2shf_map: Seq[Seq[shfHbIdx]]        = sw_shuffle(false)
+  private val seq2shf_2d_cln_map: Seq[Seq[shfHbIdx]] = sw_shuffle(true)
+
+  private val shf2seq_map: Seq[ListMap[Int, Int]] = seq2shf_map.map { map =>
+    val m = map.zipWithIndex.map {
+      case (shf, seq) =>
+        shf.idx -> seq
+    }.toMap
+
+    val res = ListMap(m.toSeq.sortBy(_._1):_*) // simply convert to ListMap
+
+    res
+  }
+  private val shf2seq_2d_cln_map: Seq[ListMap[Int, Int]] = seq2shf_2d_cln_map.map { map =>
+    val m = map.zipWithIndex.map {
+      case (shf, seq) =>
+        shf.idx -> seq
+    }.toMap
+
+    val res = ListMap(m.toSeq.sortBy(_._1):_*) // simply convert to ListMap
+
+    res
+  }
+
+// ------------------------------------------ Index classes ------------------------------------------------- //
+  /** half byte idx in the sequential word
+   *
+   * @param idx
+   */
+  class seqHbIdx(val idx: Int) {
+    require(idx < hbNum)
+
+    private val splitResultNormal = this.split(false)
+    private val splitResult2dCln  = this.split(true )
+    val idxBin = s"${String.format("%" + hbIdxBits + "s", idx.toBinaryString).replace(' ', '0')}"
+
+    // _1: normal
+    // _2: 2D Cln
+    val elemOff: (Seq[Int], Seq[Int]) = (splitResultNormal.map(_._1), splitResult2dCln.map(_._1))
+    val laneId : (Seq[Int], Seq[Int]) = (splitResultNormal.map(_._2), splitResult2dCln.map(_._3))
+    val elemIdx: (Seq[Int], Seq[Int]) = (splitResultNormal.map(_._3), splitResult2dCln.map(_._2))
+
+    private def split(is2DCln: Boolean): IndexedSeq[(Int, Int, Int)] = {
+      possibleEWs.indices.map { wid =>
+        val elemOffBits = wid
+        val elemIdxBits = hbIdxBits - elemOffBits - laneIdBits
+
+        val part1Bits = elemOffBits
+        val part2Off = part1Bits
+        val part2Bits = if (is2DCln) elemIdxBits else laneIdBits
+        val part3Off = part2Off + part2Bits
+        val part3Bits = if (is2DCln) laneIdBits else elemIdxBits
+
+        val part1 = if (part1Bits == 0) 0 else idx % (1 << part2Off)
+        val part2 = if (part1Bits == 0) idx % (1 << part3Off) else (idx % (1 << part3Off)) >> part2Off
+        val part3 = idx >> part3Off
+
+        (part1, part2, part3)
+      }
+    }
+
+    def getShfIdx(is2DCln: Boolean, wid: Int): shfHbIdx = {
+      def get(in: (Seq[Int], Seq[Int])): Int = if (is2DCln) in._2(wid) else in._1(wid)
+
+      val elemOffBits = wid
+      val elemIdxBits = hbIdxBits - elemOffBits - laneIdBits
+
+      val eOff = get(this.elemOff)
+      val eIdx = get(this.elemIdx)
+      val lane = get(this.laneId)
+
+      val laneOff = shfElemIdxMap(eIdx) + eOff // Dont need to "shfElemIdxMap(eIdx) << elemIdxBits" !
+//      println(s"laneOff=$laneOff, shfElemIdx=${shfElemIdxMap(eIdx)}, elemOff=${eOff}, elemOffBits=$elemOffBits")
+
+      new shfHbIdx(lane, laneOff)
+    }
+
+    override def toString: String = {
+      s"""
+         |[seq half byte $idx (bin: $idxBin)]
+         |Normal:
+         |\telemOff = ${elemOff._1}
+         |\tlaneId  = ${laneId._1}
+         |\telemIdx = ${elemIdx._1}
+         |
+         |2D Cln:
+         |\telemOff = ${elemOff._2}
+         |\tlaneId  = ${laneId._2}
+         |\telemIdx = ${elemIdx._2}
+         |""".stripMargin
+    }
+  }
+
+  /** half byte index in the shuffled word.
+   *
+   * @param laneId
+   * @param laneOff
+   */
+  class shfHbIdx(val laneId: Int, val laneOff: Int) {
+    val idx = (laneId << laneOffBits) + laneOff
+    val idxBin = s"${String.format("%" + hbIdxBits + "s", idx.toBinaryString).replace(' ', '0')}"
+
+    require(idx < hbNum, s"idx = $idx should always < hbNum = $hbNum. Error laneId = $laneId, laneOff = $laneOff")
+
+    override def toString: String = {
+      s"[shf_half_byte: idx = $idx (bin: $idxBin)\tlane = $laneId\tlaneOff = $laneOff]"
+    }
+  }
+
+// ------------------------------------------ sw shuffle ------------------------------------------------- //
+  /** Since the mapping relationship follows a sequential integer index starting from 0,
+   *  there is no need to implement a Map structure. A linear sequence (e.g., array or list) suffices:
+   *
+   *  Index: Represents the original position in the sequential order.
+   *  Value: Stores the target position in the shuffled order.
+   *
+   * @return shfElemIdx (0 ~ 31) -> The start hbIdx of this element.
+   *         Typically when SLEN = 128:
+   *         start hbIdx of this element: 31 30 29 28 27 26 25 24 | 23 22 21 20 19 18 17 16 | 15 14 13 12 11 10  9  8 |  7  6  5  4  3  2  1  0 |
+   *         elemIdx                    : 31 15 23  7 27 11 19  3 | 30 14 22  6 26 10 18  2 | 29 13 21  5 25  9 17  1 | 28 12 20  4 24  8 16  0 |
+   *
+   *         ListMap( 0 ->  0,  1 ->  8,  2 -> 16,  3 -> 24,  4 ->  4,  5 -> 12,  6 -> 20,  7 -> 28,
+   *                  8 ->  2,  9 -> 10, 10 -> 18, 11 -> 26, 12 ->  6, 13 -> 14, 14 -> 22, 15 -> 30,
+   *                 16 ->  1, 17 ->  9, 18 -> 17, 19 -> 25, 20 ->  5, 21 -> 13, 22 -> 21, 23 -> 29,
+   *                 24 ->  3, 25 -> 11, 26 -> 19, 27 -> 27, 28 ->  7, 29 -> 15, 30 -> 23, 31 -> 31)
+   */
+  private def shuffle_elemIdx: ListMap[Int, Int] = {
+    def recursion(seq: Seq[Int]): Seq[Int] = {
+      val threshold = SLEN / possibleEWs.max
+
+      if (seq.size == threshold) {
+        seq
+      } else {
+        val (front, back) = seq.splitAt(seq.size / 2)
+
+        val frontResult = recursion(front)
+        val backResult = recursion(back)
+
+        frontResult.zip(backResult).flatMap { case (f, b) =>
+          Seq(f, b)
+        }
+      }
+    }
+
+    val map = recursion(seqElemIdxs).zipWithIndex.map {
+      case (seq, shf) => seq -> shf
+    }.toMap
+
+    val res = ListMap(map.toSeq.sortBy(_._1):_*) // simply convert to ListMap
+
+    println(s"shuffle elemIdx map: $res")
+
+    res
+  }
+
+  /** software shuffle
+   *
+   * @param is2DCln
+   * @return Map[ EW -> Map[ seqIdx -> shfIdx ] ]
+   *         The advantage of returning in this manner is that 'seqIdx' and 'shfIdx' have a one-to-one correspondence.
+   *         During the deshuffle process, you can simply flip the key-value pairs, making it extremely convenient.
+   *
+   */
+  def sw_shuffle(is2DCln: Boolean) = {
+    println(s"[software shuffle map (is 2D cln: $is2DCln)]\n")
+    possibleEWs.indices.map { eew =>
+      val res = seqIdxs.map(idx => idx.getShfIdx(is2DCln, eew))
+
+      println(s"EW = ${1 << (eew + 2)}")
+      res.zipWithIndex.foreach {
+        case (shf, seq) =>
+          println(s"\tseqHbIdx: $seq -> $shf")
+      }
+      println("\n")
+
+      res
+    }
+  }
+
+// ------------------------------------------ hw shuffle ------------------------------------------------- //
+  /** hardware shuffle
+   *
+   * @param mode
+   * @param wid
+   * @param seqBuf
+   * @param shfBuf_r Should be a reg, because it is a vec and is given a value separately according to the idx. Chisel doesn't allow it to be wire.
+   */
+  def hw_shuffle[T <: Data](mode: VecMopOH, wid: UInt, seqBuf: Vec[T], shfBuf_r: Vec[Vec[T]]): Unit = {
+    shfBuf_r.zipWithIndex.foreach {
+      case (vec, lane) =>
+        vec.zipWithIndex.foreach {
+          case (hb, off) =>
+            val shfHbIdx = new shfHbIdx(lane, off)
+
+            /* Here, Vec is used instead of PriorityMux to ensure that
+             * the Verilog generated by Chisel is more structured and organized.
+             * Typically, the generated Verilog takes the following form:
+             *
+             * """
+             * automatic logic [3:0][3:0] _GEN_350 = {{io_seqBuf_174}, {io_seqBuf_214}, {io_seqBuf_234}, {io_seqBuf_117}};
+             * automatic logic [3:0][3:0] _GEN_351 = {{io_seqBuf_182}, {io_seqBuf_186}, {io_seqBuf_188}, {io_seqBuf_174}};
+             *
+             * shfBuf_r_5_22 <= io_mode_cln2D ? _GEN_351[io_wid] : _GEN_350[io_wid];
+             * """
+             */
+            // TODO: 合并一些重复的情况
+            // INCR, STRD, 2D_ROW Mode
+            val vec1 = VecInit(possibleEWs.indices.map { eew =>
+              seqBuf(shf2seq_map(eew)(shfHbIdx.idx))
+            })
+            // 2D_CLN Mode
+            val vec2 = VecInit(possibleEWs.indices.map { eew =>
+              seqBuf(shf2seq_2d_cln_map(eew)(shfHbIdx.idx))
+            })
+
+            hb := Mux(
+              mode.cln2D,
+              vec2(wid),
+              vec1(wid)
+            )
+
+        }
+    }
+  }
+
+  /** The Verilog file generated by the Chisel shuffle function is too long.
+   *  To make debugging clearer, it should be encapsulated as a separate module.
+   *
+   *  @tparam T UInt(4.W)(half byte) or Bundle Type
+   */
+  class shuffle[T <: Data](gen: T) extends Module {
+    val io = IO(new Bundle {
+      val mode   = Input(new VecMopOH)
+      val wid    = Input(UInt(2.W))
+      val seqBuf = Input(Vec(hbNum, gen))
+      val shfBuf = Output(Vec(NrLanes, Vec(hbNum/NrLanes, gen)))
+    })
+
+    val shfBuf_r = Reg(Vec(NrLanes, Vec(hbNum/NrLanes, gen)))
+
+    hw_shuffle(io.mode, io.wid, io.seqBuf, shfBuf_r)
+
+    io.shfBuf := shfBuf_r
+  }
+}
